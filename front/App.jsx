e8cefd1b7b3107a938fd427e68a2a4594fd00188
kayeon <kayeon8855@gmail.com>
2022-03-28 22:25:16 +0900

    update: add comment

--- front/App.jsx
+++ front/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import AddPostForm from "./components/post/AddPostForm.jsx";
@@ -7,15 +6,6 @@ import ModifyPostForm from "./components/post/ModifyPostForm.jsx";
 import PostContent from "./components/post/PostContent.jsx";
 import LoginForm from "./components/user/LoginForm";
 import SignupForm from "./components/user/SignupForm";
-=======
-import { Route, BrowserRouter, Routes, useNavigate } from "react-router-dom";
-import LoginForm from "./components/LoginForm.jsx";
-import Home from "./components/Home";
-import SignupForm from "./components/SignupForm.jsx";
-import AddPostForm from "./components/AddPostForm.jsx";
-import PostContent from "./components/PostContent.jsx";
-import ModifyPostForm from "./components/ModifyPostForm.jsx";
->>>>>>> 9028d69 (update: show posts)
 
 const App = () => {
     return (
